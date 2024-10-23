3763c304b5a0de539227d2d61c00c852cae2d354
Ranjeth <ranjethravichandran@hotmail.com>
2024-10-23 18:43:00 +0100

    Fixed Locall build and produciton build.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 
+/* Conditional statement so build can work in production and locally. */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-    basePath: '/final-portfolio',
+    basePath: isProd ? '/final-portfolio' : '',
     assetPrefix: '/final-portfolio',
     publicRuntimeConfig: {
         basePath: '/final-portfolio',
