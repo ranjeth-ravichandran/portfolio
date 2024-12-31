f1ee5e45ae5567ffc3bdaeaf8c962c069b831835
Ranjeth Ravichandran <ranjeth1965@gmail.com>
2024-12-31 09:24:41 +0000

    Editing Folder stuff renaming

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,14 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     // Use conditional basePath for local development and production
-    basePath: isProd ? '/final-portfolio' : '',
+    basePath: isProd ? '/portfolio' : '',
 
     // Remove assetPrefix or set to empty string for local development
-    assetPrefix: isProd ? '/final-portfolio/' : '',
+    assetPrefix: isProd ? '/portfolio/' : '',
 
     // Update publicRuntimeConfig
     publicRuntimeConfig: {
-        basePath: isProd ? '/final-portfolio' : '',
+        basePath: isProd ? '/portfolio' : '',
     },
 
     images: {
