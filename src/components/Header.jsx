63e0332fd27234b56dea24d1a5c4134477bef7e6
Ranjeth Ravichandran <ranjeth1965@gmail.com>
2025-01-16 17:56:45 +0000

    Fixing Images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return(
         <div className="header-container basic-container">      
             <div className="left">
-                <Image src={"/images/profile.jpg"} width={100} height={100} />
+                <Image src={"./images/profile.jpg"} width={100} height={100} />
                 <a href="resume.pdf" target="_blank">
                     <Image src={"/svg/folder.svg"} alt="Folder" width={32} height={32}/>
                     <button className="resume-button">Resume</button>
