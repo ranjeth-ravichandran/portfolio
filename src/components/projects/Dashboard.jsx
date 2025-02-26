c222cf97abde0a42c906718bc7a891bf8d3aa199
Ranjeth <ranjeth1965@gmail.com>
2025-02-26 11:24:16 +0000

    Fixing css for mobile

--- src/components/projects/Dashboard.jsx
+++ src/components/projects/Dashboard.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 
-export default function Dashboard({data}) {
+export default function Dashboard({ data }) {
     return (
         <>
-            <div style={{display: "flex", justifyContent: "center", flexWrap: "wrap"}}>
-                <Image src={data.image} width={500} height={500}/>
-                <Image src={`./images/dashboard-1.png`} width={500} height={500}/>
-                <Image src={`./images/dashboard-3.png`} width={500} height={300}/>
-            </div>
+            <div className="dashboard-images">
+                <Image src={data.image} width={500} height={500} />
+                <Image src={`./images/dashboard-1.png`} width={500} height={500} />
+                <Image src={`./images/dashboard-3.png`} width={500} height={300} />
+            </div >
             <p>{data.details}</p>
         </>
     )
